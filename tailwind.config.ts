3825a856cd552c728e4254810625bf9c2345479a
Aripilli-Bhavana <aripilli.bhavana8@gmail.com>
2025-07-19 00:54:12 +0530

    Added configurations

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -63,7 +63,7 @@ export default {
 					border: 'hsl(var(--sidebar-border))',
 					ring: 'hsl(var(--sidebar-ring))'
 				},
-				// Refuel brand colors matching the logo
+				
 				refuel: {
 					burgundy: '#8B1538',
 					'burgundy-light': '#A8324A',
